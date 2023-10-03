9190fc9300bed4663c2727de0d0df805c6fd70c6
DevMedix <rawlingsmichael01@gmail.com>
2023-10-03 15:14:54 +0100

    modified 0-simply_match_school.rb file

--- 0x06-regular_expressions/0-simply_match_school.rb
+++ 0x06-regular_expressions/0-simply_match_school.rb
@@ -1,17 +1,2 @@
 #!/usr/bin/env ruby
-
-def match_school(input)
-    regex = /School/
-    match = input.scan(regex)
-
-    if match.any?
-        puts match.join
-    else
-        puts ''
-    end
-end
-
-if ARGV.length == 1
-    input_string = ARGV[0]
-    match_school(input_string)
-end
+puts ARGV[0].scan(/School/).join
